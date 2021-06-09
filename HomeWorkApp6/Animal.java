6c50c3f6ffad74567ae5606d612c4ef2d4ebd144
ddurovv <elkeoned@gmail.com>
2021-06-09 18:41:25 +0300

    Домашняя работа 6

---
+++ HomeWorkApp6/Animal.java
@@ -0,0 +1,12 @@
+package HomeWorkApp6;
+
+public class Animal {
+    protected String name;
+    protected String color;
+    protected int age;
+    Animal(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+}
